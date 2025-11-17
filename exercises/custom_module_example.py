5d7d34b169887f7c845d05d350dafe6c865b7fda
Aditya Pratap Bhuyan <aditya.sunjava@gmail.com>
2025-11-17 10:30:58 +0530

    Create custom_module_example.py with square and cube functions
    This module provides functions to calculate the square and cube of a number.

---
+++ exercises/custom_module_example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+custom_module_example.py
+GNU GPLv3 License
+A simple demonstration of a user-defined module.
+"""
+
+def square(num):
+    """Return the square of a number."""
+    return num ** 2
+
+def cube(num):
+    """Return the cube of a number."""
+    return num ** 3
